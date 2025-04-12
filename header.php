207b5de632938993ab51a6827456dc3546944bc1
OsinachiAtama <osinachiatama@gmail.com>
2025-04-12 14:12:04 -0400

    Adding header.php

---
+++ header.php
@@ -0,0 +1,29 @@
+<?php
+    //Header
+    echo("<div id= 'top_bar'>")
+        <header>
+            <a href="index.html">
+                <div id = "img_container">
+                    <img id="logo" src="./images/house_logo.png">
+                </div>
+            </a>
+            <a href="index.html"><span id="title">PropertyPath</span></a>
+        </header>
+        //Navagation Bar//
+        <nav>
+            <span class="hamburger" onclick="toggleMenu()">&#9776;</span>
+            <div class="nav-links">
+                <a href="index.html">Home </a>
+                <a href="buy.html">Buy </a>
+                <a href="rent.html">Rent</a>
+                <a href="sell.html">Sell</a>
+                <a href="agents.html">Agents</a>
+                <a href="qanda.html">Q/A</a>
+                <a href="contact.html">Contact Us</a>
+            </div>
+        </nav>
+        <div id = "below_nav"></div>
+    </div>
+
+
+?>
