a109891d24cf5e84f7a746c1fc22fc93f15da211
Mayha Ali <mali10@eecs.tufts.edu>
2025-04-24 12:04:32 -0400

    package.json and server.js

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+app.use(express.static(__dirname));
+const URL = 'mongodb+srv://mayhaali:SsLi5KuHuTDNaWK6@cluster0.j8ysx6r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const { MongoClient } = require('mongodb');
+const path = require('path');
+
+const PORT = 3000;
+
+let db, collection;
+
+// Connect to MongoDB and start the server
+MongoClient.connect(URL)
+  .then(client => {
+    db = client.db('cookbook');
+    collection = db.collection('recipes');
+    app.listen(PORT);
+  })
+  .catch(err => console.error('MongoDB connection error:', err));
+
+// fetch all data
+app.get('/data', async (req, res) => {
+  try {
+    const recipes = await collection.find().toArray();
+    res.json(recipes);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to fetch recipes' });
+  }
+});
