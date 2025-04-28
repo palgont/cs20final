e144c3f2e18e1d61c1eeb42cecf0de2ea97ca7f3
OsinachiAtama <osinachiatama@gmail.com>
2025-04-27 22:29:55 -0400

    Set up mongodb stuff to read from database

---
+++ mongo_connect.js
@@ -0,0 +1,22 @@
+const {MongoClient} = require('mongodb');
+var readline = require('readline'); 
+var fs = require('fs'); 
+
+const connStr= "mongodb+srv://Osinachi:lordofhosts@cluster0.enps8.mongodb.net/"
+const client = new MongoClient(connStr);
+
+async function run(){ 
+  try { 
+    await client.connect();
+    console.log("Connected to mongo!")
+    var dbo = client.db("Stock");
+    return dbo.collection('PublicCompanies');     
+
+    } catch (dbErr){ 
+        console.log("Database error: " + dbErr);
+        process.exit(1); 
+    }
+}
+run();
+module.exports = connectDB;
+
